9675e721eb7e03af7113a1fbd2aead9bac1c4cfa
Fred Straub <50086431+fredstraub@users.noreply.github.com>
2022-12-06 16:09:03 -0800

    update

--- test2.pp
+++ test2.pp
@@ -17,19 +17,25 @@ package { 'python':
   provider => pip3
 }
 
+# python3 --version
+
 package {'pip3':
   require  => Package['python3-pip'],
   ensure   => installed,
   provider => 'pip3'
 }
+# pip3 --version
 
 $packages = ['python3-pip', 'apache2', 'libapache2-mod-wsgi']
 
 package { $packages:
   ensure => installed
 }
+# /usr/sbin/apache2 -v
+# dpkg -s libapache2-mod-wsgi
 
 package { bottle:
   ensure   => '0.10.11',
   provider => 'pip3',
 }
+# python3 -m bottle --version
